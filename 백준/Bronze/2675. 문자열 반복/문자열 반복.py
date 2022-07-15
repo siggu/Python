096fddd439dfd518ec429edbce010bcb05979ff8
siggu <106001755+siggu@users.noreply.github.com>
2022-07-15 11:18:55 +0900

    [Bronze II] Title: 문자열 반복, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for i in range(T):
+    R,S = map(str, input().split())
+    R = int(R)
+    S = str(S)
+        
+    for i in range(len(S)):
+        print(R*S[i], end='')
+    print()
