8549a37cf075930d7ea2a17902d4e1f7a14a8fa7
siggu <106001755+siggu@users.noreply.github.com>
2022-07-16 13:06:46 +0900

    [Bronze IV] Title: 주사위 세개, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,12 @@
+x, y, z = map(int, input().split())
+    
+if x == y and y == z:
+    print(10000 + (x * 1000))
+elif x == y and y != z:
+    print(1000 + (x * 100))
+elif x == z and y != z:
+    print(1000 + (x * 100))
+elif y == z and x != y:
+    print(1000 + (y * 100))
+else:
+    print(max(x, y, z) * 100)
