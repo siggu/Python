588dfc081128a4c6c791e24856cc6b800ae7e8f0
siggu <106001755+siggu@users.noreply.github.com>
2022-11-10 00:26:59 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,14 @@
+def solution(num_list):
+    even, odd = 0, 0
+    arr = []
+    
+    for i in num_list:
+        if i % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    
+    arr.append(even)
+    arr.append(odd)
+    
+    return arr
