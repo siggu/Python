3d21e8d1d448c60e7a27847206b5ca80b4bc39ff
siggu <106001755+siggu@users.noreply.github.com>
2022-11-20 13:00:36 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.14 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = ""
+    
+    for i in my_string:
+        if i.isupper():
+            answer += i.lower()
+        else:
+            answer += i.upper()
+            
+    return answer
