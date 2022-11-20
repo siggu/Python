0acddd086d3995b27abc1f8ed77c94e176858a2d
siggu <106001755+siggu@users.noreply.github.com>
2022-11-20 13:36:03 +0900

    [Silver V] Title: 거스름돈, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,18 @@
+import sys
+read = sys.stdin.readline
+
+n = int(read())
+cnt = 0
+
+while n > 0:
+    if n % 5 == 0:
+        cnt += n // 5
+        break
+    else:
+        n -= 2
+        cnt += 1
+    
+if n < 0:
+    print(-1)
+else:
+    print(cnt)
