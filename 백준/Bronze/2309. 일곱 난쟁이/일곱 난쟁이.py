cc6786b7870954cc97cf184a99a4003380447b3d
siggu <106001755+siggu@users.noreply.github.com>
2022-12-08 00:35:22 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,11 @@
+height = []
+for i in range(9):
+    height.append(int(input()))
+for i in height:
+    for j in height:
+        if sum(height) - i - j == 100 and i != j:
+            height.remove(i)
+            height.remove(j)
+height.sort()            
+for i in height:
+    print(i)
