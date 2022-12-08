f5fd455467a9657520fc5c66533d6448193a7512
siggu <106001755+siggu@users.noreply.github.com>
2022-12-08 13:52:38 +0900

    [Silver IV] Title: 로프, Time: 4584 ms, Memory: 38128 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,13 @@
+n = int(input())
+rope = []
+value = []
+
+for i in range(n):
+    rope.append(int(input()))
+    
+rope.sort(reverse=True)
+
+for i in range(n):
+    value.append(rope[i] * (i+1))
+
+print(max(value))
