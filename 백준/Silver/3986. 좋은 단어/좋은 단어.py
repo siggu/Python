b30707bfdb7fca55faa8a31c246e38500982d13d
siggu <106001755+siggu@users.noreply.github.com>
2023-01-28 19:05:13 +0900

    [Silver IV] Title: 좋은 단어, Time: 120 ms, Memory: 30776 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -0,0 +1,17 @@
+n = int(input())
+cnt = 0
+ 
+for _ in range(n):
+    s = input()
+    stack = []
+ 
+    for i in range(len(s)):
+        if stack and s[i] == stack[-1]:
+            stack.pop()
+        else:
+            stack.append(s[i])
+ 
+    if not stack:
+        cnt += 1
+        
+print(cnt)
