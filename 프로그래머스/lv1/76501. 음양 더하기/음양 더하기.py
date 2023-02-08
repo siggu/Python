db5e13bd90476b62dc16178d804f8e41c5c74f16
siggu <106001755+siggu@users.noreply.github.com>
2023-02-08 21:24:34 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,10 @@
+def solution(absolutes, signs):
+    result = 0
+    
+    for i in range(len(signs)):
+        if signs[i]:
+            result += absolutes[i]
+        else:
+            result -= absolutes[i]
+    
+    return result
