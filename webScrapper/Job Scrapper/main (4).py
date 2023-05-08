2223f5c4cd3a2d64e8d538f784a77bbf71fdd5af
siggu <106001755+siggu@users.noreply.github.com>
2023-05-08 13:27:51 +0900

    Add files via upload

---
+++ webScrapper/Job Scrapper/main (4).py
@@ -0,0 +1,33 @@
+# https://weworkremotely.com/
+"""
+1. 파이썬을 이용해 사이트에 접속하기
+2. jobs 라는 class를 가진 section 찾기
+3. section 안에 있는 ul 안의 li만 가져오기
+4. 데이터(a href: company, title, position, region, ... 등) 추출하기
+"""
+from requests import get
+from bs4 import BeautifulSoup
+
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+
+search_term = "python"
+
+response = get(f"{base_url}{search_term}")
+
+if response.status_code != 200:
+  print("Can't request website")
+else:
+  soup = BeautifulSoup(response.text, "html.parser")
+  jobs = soup.find_all("section", class_="jobs")
+
+  for job_section in jobs:
+    job_posts = job_section.find_all("li")
+    job_posts.pop(-1)
+    for post in job_posts:
+      anchors = post.find_all("a")
+      anchor = anchors[1]
+      link = anchor["href"]
+      company, kind, region = anchor.find_all("span", class_="company")
+      title = anchor.find("span", class_="title")
+      print(company, kind, region, title)
+      print()
