e8a1f06d5da43470d92bb248ade0553750dfe576
siggu <106001755+siggu@users.noreply.github.com>
2023-11-10 13:53:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 비밀지도 - 17681 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17681) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.07 ms
+메모리: 10.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 5일 13:51:39
+2023년 11월 5일 13:54:4
 
 ### 문제 설명
 
--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,32 +1,17 @@
-def solution(n, arr1, arr2): # n: 정사각형 한 변의 길이, arr1: 지도1, arr2: 지도2
-    # [1, 2, 4, 8, 16, 32,  ... 2^15] (1 <= n <= 16)
-    
+def solution(n, arr1, arr2):
     answer = []
     
     for i in range(n):
         word = ""
-        if len(bin(arr1[i])[2:]) != n:
-            arr1_column = bin(arr1[i])[2:].zfill(n)
-        else:
-            arr1_column = bin(arr1[i])[2:]
-        
-        if len(bin(arr2[i])[2:]) != n:
-            arr2_column = bin(arr2[i])[2:].zfill(n)
-        else:
-            arr2_column = bin(arr2[i])[2:]
-        
-        print(f'arr1_column = {arr1_column}, arr2_column = {arr2_column}')
-        
-        # print(int(arr1_column[0]) + int(arr2_column[0]))
-        
+        arr1_column = bin(arr1[i])[2:].zfill(n)
+        arr2_column = bin(arr2[i])[2:].zfill(n)
         
         for j in range(n):
             if int(arr1_column[j]) + int(arr2_column[j]) >= 1:
                 word += "#"
-            elif int(arr1_column[j]) + int(arr2_column[j]) == 0:
+            else:
                 word += " "
         
         answer.append(word)
 
-        
     return answer
