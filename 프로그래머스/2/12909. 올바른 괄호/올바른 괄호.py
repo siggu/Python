5d07a54551918773dc111da51917690daa2baf89
siggu <106001755+siggu@users.noreply.github.com>
2023-12-02 13:58:00 +0900

    [level 2] Title: 올바른 괄호, Time: 7.68 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,15 @@
+def solution(s):
+    cnt = 0
+    
+    for i in s:
+        if i == "(":
+            cnt += 1
+        elif i == ")":
+            cnt -= 1
+        if cnt < 0:
+            return False
+    
+    if cnt != 0:
+        return False
+    
+    return True
